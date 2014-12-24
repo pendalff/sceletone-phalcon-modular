bcdbaea08983434fb3db4df39e591e3097efd095
Sem Fukalov <s.fukalov@work5.ru>
2014-12-24 15:00:06 +0600

    Added real used materials for partner module

---
+++ www/static/js/jquery.main.js
@@ -0,0 +1,55 @@
+$(document).ready(function(){
+	if ($('input, textarea').size()) {
+		$('input, textarea').placeholder();
+	}
+	$("#phone").mask("(999) 999-9999");
+	$( "#datepicker" ).datepicker();
+	$('.tooltip-btn').tooltip();
+	$('.popover-btn').popover();
+	var availableTags = [
+		"ActionScript",
+		"AppleScript",
+		"Asp",
+		"BASIC",
+		"C",
+		"C++",
+		"Clojure",
+		"COBOL",
+		"ColdFusion",
+		"Erlang",
+		"Fortran",
+		"Groovy",
+		"Haskell",
+		"Java",
+		"JavaScript",
+		"Lisp",
+		"Perl",
+		"PHP",
+		"Python",
+		"Ruby",
+		"Scala",
+		"Scheme"
+		];
+		$( "#tags" ).autocomplete({
+		source: availableTags
+	});
+	jQuery.datepicker.regional['ru'] = {
+	closeText: 'Закрыть',
+	prevText: '&#x3c;Пред',
+	nextText: 'След&#x3e;',
+	currentText: 'Сегодня',
+	monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь',
+	'Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+	monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн',
+	'Июл','Авг','Сен','Окт','Ноя','Дек'],
+	dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+	dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+	dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+	weekHeader: 'Не',
+	dateFormat: 'dd.mm.yy',
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: ''};
+	jQuery.datepicker.setDefaults(jQuery.datepicker.regional['ru']);
+});
